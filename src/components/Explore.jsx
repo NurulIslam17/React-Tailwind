1b2b6a0c11202951afb77087c988e45d9933dec5
@Nurul97 <nurulislamnextgenitltd.com>
2023-10-17 22:01:21 +0600

    ok

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -5,12 +5,12 @@ function Explore() {
   return (
     <div className='max-w-[1240px] mx-auto py-5 md:grid grid-cols-2'>
         <div className='col-span-1 w-[80%]'>
-            <img src={explore} alt="" srcset="" />
+            <img src={explore} alt="explore" />
         </div>
         <div className='col-span-1 flex flex-col p-3'>
             <h1 className='font-bold text-2xl text-gray-400 py-3 text-center md:text-left'>Explore Some code</h1>
             <p className='text-justify mb-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus tenetur quae qui cumque doloribus illo, at minus earum architecto ratione autem molestias accusantium consequatur labore, perferendis quod fuga. Ullam, nisi.</p>
-            <button type="button" class="w-[40%] md:w-[15%] font-bold py-1 px-5 mr-2 mb-2 text-sm text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Explore</button>
+            <button type="button" className="w-[40%] md:w-[15%] font-bold py-1 px-5 mr-2 mb-2 text-sm text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Explore</button>
         </div>
     </div>
   )
